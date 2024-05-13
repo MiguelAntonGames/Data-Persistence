85cd3fd9844700d3ff1f5fc0319d369be375acc2
AntonMiguel <miguelantongames@gmail.com>
2024-05-13 16:56:30 -0300

    Small gameplay improvement
    The delay before brick destruction has been reduced from 0.2s to 0.16s

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,22 +1,17 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Brick : MonoBehaviour
-{
+public class Brick : MonoBehaviour {
     public UnityEvent<int> OnDestroyed;
-    
     public int PointValue;
 
-    void Start()
-    {
+    void Start() {
         var renderer = GetComponentInChildren<Renderer>();
 
-        MaterialPropertyBlock block = new MaterialPropertyBlock();
-        switch (PointValue)
-        {
+        MaterialPropertyBlock block = new();
+        switch (PointValue) {
             case 1 :
                 block.SetColor("_BaseColor", Color.green);
                 break;
@@ -33,11 +28,10 @@ public class Brick : MonoBehaviour
         renderer.SetPropertyBlock(block);
     }
 
-    private void OnCollisionEnter(Collision other)
-    {
+    private void OnCollisionEnter(Collision other) {
         OnDestroyed.Invoke(PointValue);
-        
-        //slight delay to be sure the ball have time to bounce
-        Destroy(gameObject, 0.2f);
+
+        //slight delay to be sure that the ball has time to bounce
+        Destroy(gameObject, 0.16f);
     }
 }
